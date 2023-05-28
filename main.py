72f745036d15568369bbd302e117832c2740a68c
PedroNascimento792 <pedrocapedrocas@gmail.com>
2023-05-27 21:20:02 -0300

    resolvido

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """Main functions"""
-
+import re
 
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
-    return False
+    string = re.sub(r"\W+", "", string.lower())
+    return string == string[::-1]
+    
